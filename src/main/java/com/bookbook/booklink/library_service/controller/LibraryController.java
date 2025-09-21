75ccf7f2154dee7ba483b0c825f37b077466cdd1
scy <tjcksdud0221@naver.com>
2025-09-22 00:45:58 +0900

    :recycle: refactor: Renaming example variables for BaseResponse's error and success.

--- src/main/java/com/bookbook/booklink/library_service/controller/LibraryController.java
+++ src/main/java/com/bookbook/booklink/library_service/controller/LibraryController.java
@@ -39,7 +39,7 @@ public class LibraryController {
                                     schema = @Schema(implementation = BaseResponse.class),
                                     examples = @ExampleObject(
                                             name = "successResponse",
-                                            value = BaseResponse.LIBRARY_SUCCESS_RESPONSE
+                                            value = BaseResponse.SUCCESS_RESPONSE
                                     )
                             )
                     ),
@@ -51,7 +51,7 @@ public class LibraryController {
                                     schema = @Schema(implementation = BaseResponse.class),
                                     examples = @ExampleObject(
                                             name = "errorResponse",
-                                            value = BaseResponse.LIBRARY_ERROR_RESPONSE
+                                            value = BaseResponse.ERROR_RESPONSE
                                     )
                             )
                     ),
@@ -63,7 +63,7 @@ public class LibraryController {
                                     schema = @Schema(implementation = BaseResponse.class),
                                     examples = @ExampleObject(
                                             name = "errorResponse",
-                                            value = BaseResponse.LIBRARY_ERROR_RESPONSE
+                                            value = BaseResponse.ERROR_RESPONSE
                                     )
                             )
                     )

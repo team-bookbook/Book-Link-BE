78e6dceab0f4f3933c174d9606449aaec5b5f441
scy <tjcksdud0221@naver.com>
2025-09-23 18:23:55 +0900

    :sparkles: feat: Review update api 생성

--- src/main/java/com/bookbook/booklink/review_service/controller/ReviewController.java
+++ src/main/java/com/bookbook/booklink/review_service/controller/ReviewController.java
@@ -3,11 +3,13 @@ package com.bookbook.booklink.review_service.controller;
 import com.bookbook.booklink.common.exception.BaseResponse;
 import com.bookbook.booklink.review_service.controller.docs.ReviewApiDocs;
 import com.bookbook.booklink.review_service.model.dto.request.ReviewCreateDto;
+import com.bookbook.booklink.review_service.model.dto.request.ReviewUpdateDto;
 import com.bookbook.booklink.review_service.service.ReviewService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +37,26 @@ public class ReviewController implements ReviewApiDocs {
 
         log.info("[ReviewController] [traceId = {}, userId = {}] create review response success. targetId={}",
                 traceId, userId, reviewCreateDto.getTarget_id());
+
+        return ResponseEntity.ok()
+                .body(BaseResponse.success(Boolean.TRUE));
+    }
+
+    @Override
+    public ResponseEntity<BaseResponse<Boolean>> updateReview(
+            @PathVariable UUID reviewId,
+            @Valid @RequestBody ReviewUpdateDto reviewUpdateDto,
+            @RequestHeader("Trace-Id") String traceId
+    ) {
+        UUID userId = UUID.randomUUID();
+
+        log.info("[ReviewController] [traceId = {}, userId = {}] update review request received. reviewId={}",
+                traceId, userId, reviewId);
+
+        reviewService.updateReview(reviewUpdateDto, reviewId, traceId, userId);
+
+        log.info("[ReviewController] [traceId = {}, userId = {}] update review request success. reviewId={}",
+                traceId, userId, reviewId);
 
         return ResponseEntity.ok()
                 .body(BaseResponse.success(Boolean.TRUE));
--- src/main/java/com/bookbook/booklink/review_service/controller/docs/ReviewApiDocs.java
+++ src/main/java/com/bookbook/booklink/review_service/controller/docs/ReviewApiDocs.java
@@ -5,14 +5,14 @@ import com.bookbook.booklink.common.exception.ApiErrorResponses;
 import com.bookbook.booklink.common.exception.BaseResponse;
 import com.bookbook.booklink.common.exception.ErrorCode;
 import com.bookbook.booklink.review_service.model.dto.request.ReviewCreateDto;
+import com.bookbook.booklink.review_service.model.dto.request.ReviewUpdateDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RequestMapping("/api/review")
 @Tag(name = "Review API", description = "리뷰 등록/조회/수정 관련 API")
@@ -30,6 +30,17 @@ public interface ReviewApiDocs {
     @PostMapping
     public ResponseEntity<BaseResponse<Boolean>> createReview(
             @Valid @RequestBody ReviewCreateDto reviewCreateDto,
+            @RequestHeader("Trace-Id") String traceId
+    );
+
+    @Operation(
+            summary = "리뷰 수정",
+            description = "사용자/도서관에 대한 리뷰를 수정합니다. 평점, 코멘트 수정가능"
+    )
+    @PutMapping("/{reviewId}")
+    public ResponseEntity<BaseResponse<Boolean>> updateReview(
+            @PathVariable UUID reviewId,
+            @Valid @RequestBody ReviewUpdateDto reviewUpdateDto,
             @RequestHeader("Trace-Id") String traceId
     );
 }

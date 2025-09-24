e93d16ed3909c483eb680054869f43a346788443
Jun <wnsur1234@gmail.com>
2025-09-25 02:53:25 +0900

    :wip: Chore: Change Repository extends type to UUID

--- src/main/java/com/bookbook/booklink/common/jwt/repository/RefreshTokenRepository.java
+++ src/main/java/com/bookbook/booklink/common/jwt/repository/RefreshTokenRepository.java
@@ -4,8 +4,9 @@ import com.bookbook.booklink.common.jwt.model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
     Optional<RefreshToken> findByEmail(String email);
     void deleteByEmail(String email);
 }

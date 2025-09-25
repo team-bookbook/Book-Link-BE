71ebf0eb3bca93dc9a33e0e80435cf0122f34885
sezeme <stellaa223@gmail.com>
2025-09-26 03:38:26 +0900

    :recycle: refactor: 책 검색, 등록 api 분리 - add swagger description

--- src/main/java/com/bookbook/booklink/book_service/model/dto/response/BookResponseDto.java
+++ src/main/java/com/bookbook/booklink/book_service/model/dto/response/BookResponseDto.java
@@ -1,20 +1,36 @@
 package com.bookbook.booklink.book_service.model.dto.response;
 
 import com.bookbook.booklink.book_service.model.BookCategory;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 @Setter
+@Builder
+@Schema(description = "도서 등록을 위한 도서 조회 응답 DTO")
 public class BookResponseDto {
+    @Schema(description = "도서 고유 ID (UUID)", example = "550e8400-e29b-41d4-a716-446655440000", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private UUID id;
+    @Schema(description = "도서 이름", example = "마흔에 읽는 쇼펜하우어", requiredMode = Schema.RequiredMode.REQUIRED, minLength = 1, maxLength = 64)
     private String title;
+    @Schema(description = "저자명", example = "강용수", requiredMode = Schema.RequiredMode.REQUIRED, minLength = 1, maxLength = 16)
     private String author;
+    @Schema(description = "출판사", example = "유노북스", requiredMode = Schema.RequiredMode.REQUIRED, minLength = 1, maxLength = 16)
     private String publisher;
+    @Schema(description = "카테고리", example = "GENERALITIES", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private BookCategory category;
+    @Schema(description = "ISBN 코드", example = "9791192300818", requiredMode = Schema.RequiredMode.REQUIRED)
     private String ISBN;
-    private String originalPrice;
-    private String publishedDate;
+    @Schema(description = "도서 정가", example = "17000", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Integer originalPrice;
+    @Schema(description = "도서 발행일", example = "2025-09-22T12:00:00", requiredMode = Schema.RequiredMode.REQUIRED)
+    private LocalDateTime publishedDate;
+
+    @Schema(description = "우리 데이터베이스에서 가져오면 true, national library에서 가져오면 false", example = "true", requiredMode = Schema.RequiredMode.REQUIRED)
+    private boolean hasFoundFromOurDatabase;
 }

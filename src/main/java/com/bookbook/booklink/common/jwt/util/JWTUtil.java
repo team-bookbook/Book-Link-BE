e04bc41c185d2ac92ca81e194b794427570b6d7f
Jun <wnsur1234@gmail.com>
2025-09-26 00:26:57 +0900

    :recycle: refactor: Use Base64 decoding for JWT secret key

--- src/main/java/com/bookbook/booklink/common/jwt/util/JWTUtil.java
+++ src/main/java/com/bookbook/booklink/common/jwt/util/JWTUtil.java
@@ -3,12 +3,12 @@ package com.bookbook.booklink.common.jwt.util;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
@@ -23,7 +23,7 @@ public class JWTUtil {
             @Value("${jwt.access-token-expiration}") long accessTokenExpiration,
             @Value("${jwt.refresh-token-expiration}") long refreshTokenExpiration
     ) {
-        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
         this.accessTokenExpiration = accessTokenExpiration;
         this.refreshTokenExpiration = refreshTokenExpiration;
     }

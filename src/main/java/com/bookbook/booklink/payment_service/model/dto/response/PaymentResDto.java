3badd0f227b7bc7bdc84ecbb72ac52bdcce618c7
scy <tjcksdud0221@naver.com>
2025-09-27 01:08:38 +0900

    :bulb: comment: 주석 추가

--- src/main/java/com/bookbook/booklink/payment_service/model/dto/response/PaymentResDto.java
+++ src/main/java/com/bookbook/booklink/payment_service/model/dto/response/PaymentResDto.java
@@ -3,6 +3,7 @@ package com.bookbook.booklink.payment_service.model.dto.response;
 import com.bookbook.booklink.payment_service.model.Payment;
 import com.bookbook.booklink.payment_service.model.PaymentMethod;
 import com.bookbook.booklink.payment_service.model.PaymentStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,14 +15,31 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "결제 결과 응답 데이터")
 public class PaymentResDto {
 
+    @Schema(
+            description = "결제 금액",
+            example = "15000"
+    )
     private Integer amount;
 
+    @Schema(
+            description = "결제 수단",
+            example = "CARD"
+    )
     private PaymentMethod paymentMethod;
 
+    @Schema(
+            description = "결제 상태",
+            example = "APPROVED"
+    )
     private PaymentStatus status;
 
+    @Schema(
+            description = "결제 생성 시각",
+            example = "2025-09-26T19:20:30"
+    )
     private LocalDateTime createdAt;
 
     public static PaymentResDto fromEntity(Payment payment) {
@@ -32,5 +50,4 @@ public class PaymentResDto {
                 .createdAt(payment.getCreatedAt())
                 .build();
     }
-
 }

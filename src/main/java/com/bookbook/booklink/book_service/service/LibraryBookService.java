8150deac623f7802e9ded678fdf7a1c096361bbd
scy <tjcksdud0221@naver.com>
2025-09-29 21:20:59 +0900

    :bulb: comment: 주석 추가

--- src/main/java/com/bookbook/booklink/book_service/service/LibraryBookService.java
+++ src/main/java/com/bookbook/booklink/book_service/service/LibraryBookService.java
@@ -11,11 +11,11 @@ import com.bookbook.booklink.common.exception.ErrorCode;
 import com.bookbook.booklink.common.service.IdempotencyService;
 import com.bookbook.booklink.library_service.model.Library;
 import com.bookbook.booklink.library_service.service.LibraryService;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -84,6 +84,13 @@ public class LibraryBookService {
     }
 
 
+    /**
+     * 특정 도서관의 Top 5 도서 목록을 반환하는 메서드
+     *
+     * @param libraryId 조회할 도서관의 ID
+     * @return 해당 도서관의 가장 인기가 많은 도서 5개 리스트
+     */
+    @Transactional(readOnly = true)
     public List<LibraryBook> findTop5Books(UUID libraryId) {
         return libraryBookRepository.findTop5BooksByLibraryOrderByLikeCount(libraryId, PageRequest.of(0, 5));
     }

ff1d7959ef7132293a675bdcaafa037c1809bda8
sezeme <stellaa223@gmail.com>
2025-09-30 01:48:36 +0900

    :recycle: refactor: 필요없는 client 제거

--- src/main/java/com/bookbook/booklink/common/config/AwsS3Config.java
+++ src/main/java/com/bookbook/booklink/common/config/AwsS3Config.java
@@ -10,27 +10,6 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class AwsS3Config {
 
-    // todo : 논의 결과에 따라 s3 client 삭제 ( s3 등록 로직을  프론트에서 할지 백엔드에서 할지 )
-    /**
-     * S3Client를 빈으로 생성합니다.
-     *
-     * <p>
-     * 서버에서 직접 S3 API를 호출할 때 사용합니다.
-     * 예: 파일 업로드, 다운로드, 삭제 등
-     * DefaultCredentialsProvider를 사용하여 환경변수, AWS CLI 프로파일,
-     * 또는 IAM 역할에서 자격증명을 자동으로 가져옵니다.
-     * </p>
-     *
-     * @return S3Client 인스턴스
-     */
-    @Bean
-    public S3Client s3Client() {
-        return S3Client.builder()
-                .region(Region.AP_NORTHEAST_2) // 서울 리전 예시
-                .credentialsProvider(DefaultCredentialsProvider.create()) // 환경변수에서 자동으로 key 찾아서 사용
-                .build();
-    }
-
     /**
      * S3Presigner를 빈으로 생성합니다.
      *

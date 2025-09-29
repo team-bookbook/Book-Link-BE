5d3465811fe3573b163b01c3ed4473b155c9a770
sezeme <stellaa223@gmail.com>
2025-09-30 02:33:22 +0900

    :sparkles: feat: 정렬 조건 및 위치정보 추가

--- src/main/java/com/bookbook/booklink/book_service/model/dto/request/LibraryBookSearchReqDto.java
+++ src/main/java/com/bookbook/booklink/book_service/model/dto/request/LibraryBookSearchReqDto.java
@@ -3,11 +3,16 @@ package com.bookbook.booklink.book_service.model.dto.request;
 import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Getter
 @Setter
 public class LibraryBookSearchReqDto {
     // todo : swagger 설명 추가
+
+    Double latitude;
+
+    Double longitude;
 
     @Min(value = 0, message = "페이지 번호는 0 이상이어야 합니다.")
     private int page = 0; // 기본값 0
@@ -16,4 +21,7 @@ public class LibraryBookSearchReqDto {
     private int size = 10; // 기본값 10
 
     private String bookName; // 검색어 (nullable)
+
+    private LibraryBookSortType sortType = LibraryBookSortType.DISTANCE; // 정렬 조건, 기본값 거리순
+
 }

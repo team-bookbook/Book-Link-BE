4be04e7124556729573b345a0e425ef4e48ccfd1
sezeme <stellaa223@gmail.com>
2025-10-09 13:10:40 +0900

    :sparkles: feat: 24시간 경과 시 자동 만료 로직 추가
    - 한시간마다 만료 처리

--- src/main/java/com/bookbook/booklink/borrow_service/scheduler/BorrowScheduler.java
+++ src/main/java/com/bookbook/booklink/borrow_service/scheduler/BorrowScheduler.java
@@ -2,12 +2,16 @@ package com.bookbook.booklink.borrow_service.scheduler;
 
 import com.bookbook.booklink.borrow_service.model.Borrow;
 import com.bookbook.booklink.borrow_service.model.BorrowStatus;
+import com.bookbook.booklink.borrow_service.model.Reservation;
+import com.bookbook.booklink.borrow_service.model.ReservationStatus;
 import com.bookbook.booklink.borrow_service.repository.BorrowRepository;
+import com.bookbook.booklink.borrow_service.repository.ReservationRepository;
 import com.bookbook.booklink.point_service.model.TransactionType;
 import com.bookbook.booklink.point_service.model.dto.request.PointUseDto;
 import com.bookbook.booklink.point_service.service.PointService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +19,14 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class BorrowScheduler {
 
     private final BorrowRepository borrowRepository;
     private final PointService pointService;
+    private final ReservationRepository reservationRepository;
 
     /**
      * 매일 자정(00:00)에 실행
@@ -52,5 +58,26 @@ public class BorrowScheduler {
                     .build();
             pointService.usePoint(pointUseDto, UUID.randomUUID(), UUID.fromString("spring-scheduler"));
         });
+    }
+
+    /**
+     * 매 정시 0분 0초에 실행
+     */
+    @Scheduled(cron = "0 0 * * * *")
+    @Transactional
+    public void markOverdueReservations() {
+        LocalDateTime now = LocalDateTime.now();
+
+        // 1. 만료된 예약 조회
+        List<Reservation> expiredReservations = reservationRepository
+                .findByStatusAndAvailableUntilBefore(ReservationStatus.AVAILABLE, now);
+
+        // 2. 상태 업데이트
+        for (Reservation reservation : expiredReservations) {
+            reservation.updateStatus(ReservationStatus.EXPIRED);
+            // todo : 알림 전송
+        }
+
+        log.info("[ReservationService] Marked {} reservations as expired", expiredReservations.size());
     }
 }
